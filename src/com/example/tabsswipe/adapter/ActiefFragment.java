922061aae6229d90731a19b47ef38bc3dc5baa14
Ali <alimranbay@gmail.com>
2015-03-31 17:10:49 +0200

    5

--- src/com/example/tabsswipe/adapter/ActiefFragment.java
+++ src/com/example/tabsswipe/adapter/ActiefFragment.java
@@ -4,20 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.example.myapp.ActieveAgendaModule;
 import com.example.myapp.ActieveDossierModule;
+import com.example.myapp.ModuleRow;
+import com.example.myapp.R;
 
 public class ActiefFragment extends ListFragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		String[] values = new String[] { "plaatsing speelplaats", "vergroening", "vermeerderen van patrouilles",
-				"plaatsing van feestzaal", "gebouwen renoveren of vervangen", "indeling verkeer",
-				"investeren in verwijderen van grafitti", "nieuwe weg", "investeren in nieuwe trams", "camerabewaking" };
-		/*ArrayAdapter<String> adapter = new MyArrayAdapter(getActivity(), values);
-
-		setListAdapter(adapter);*/
+		ModuleRow[] values = new ModuleRow[] { new ModuleRow("Plaatsing speelplaats", "Description", R.drawable.ic_launcher) };
+		ArrayAdapter<ModuleRow> adapter = new MyArrayAdapter(getActivity(), values);
+		setListAdapter(adapter);
 	}
 
 	@Override
